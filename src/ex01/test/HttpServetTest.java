efa870753ffc78e7e4ecb2552fcfd3ed5d63710e
gezhizheng <gezhizheng@mvp01.com>
2017-08-17 19:57:56 +0800

    Initial commit

---
+++ src/ex01/test/HttpServetTest.java
@@ -0,0 +1,14 @@
+package ex01.test;
+
+import org.junit.Test;
+
+/**
+ * Created by gezz on 2017/8/17.
+ */
+public class HttpServetTest {
+
+    @Test
+    public void testSystemProperty() {
+        System.out.println(System.getProperty("user.dir"));
+    }
+}
